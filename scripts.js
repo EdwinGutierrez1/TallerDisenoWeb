675491766efd3bbf111745f209e45e1b67c5d213
EdwinGutierrez1 <egutierrez11@udi.edu.co>
2025-03-07 22:14:04 -0500

    pagina al 70%

--- scripts.js
+++ scripts.js
@@ -1,63 +1,88 @@
-const navLinks = document.querySelectorAll('.navbar a');
 
-// Recorrer los enlaces y agregar eventos para cada uno
+/* Código relacionado con el Navbar */
+
+const navLinks = document.querySelectorAll('.navbar a'); // Selecciona todos los enlaces dentro del navbar
+
+// Recorre los enlaces y agregar eventos para cada uno
 navLinks.forEach(link => {
-    // Evento mouseenter para agregar la clase
+    //Evento mouseenter para agregar la clase
     link.addEventListener('mouseenter', () => {
-        link.classList.add('efecto-hover');
+        if (link.id !== 'logoPagina') { //Evita que el logo de la página tenga el efecto hover
+            link.classList.add('efecto-hover'); //Se agrega la clase de efecto hover
+        }
     });
 
     // Evento mouseout para quitar la clase
     link.addEventListener('mouseout', () => {
-        link.classList.remove('efecto-hover');
+        link.classList.remove('efecto-hover'); // Elimina la clase de efecto hover al salir del enlace
     });
 });
 
-/* Script relacionado con el funcionamiento del carrusel */
-let lista = document.querySelector('.carrusel .lista');
-let items = document.querySelectorAll('.carrusel .lista .item')
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
 
-let activa = 0; 
-let longitudItems = items.length - 1;
+/* Código relacionado con el funcionamiento del carrusel */
 
+let lista = document.querySelector('.carrusel .lista'); //Obtiene el contenedor de los elementos del carrusel
+let items = document.querySelectorAll('.carrusel .lista .item'); // Selecciona todos los elementos del carrusel
+let prev = document.getElementById('prev'); // Botón para retroceder en el carrusel
+let next = document.getElementById('next'); // Botón para avanzar en el carrusel
+let headerLinks = document.querySelectorAll('header a'); // Selecciona todos los enlaces dentro del header
 
-next.onclick = function(){
-    if(activa + 1 > longitudItems){
-        activa= 0;
-    }else{
-        activa = activa + 1;
+let activa = 0; // Índice de la imagen actualmente visible en el carrusel
+let longitudItems = items.length - 1; // Cantidad total de elementos en el carrusel (restando 1 para manejar los índices correctamente)
+
+// Evento al hacer clic en el botón "next"
+next.onclick = function() {
+    if (activa + 1 > longitudItems) { // Si se llega al último elemento, vuelve al inicio
+        activa = 0;
+    } else {
+        activa = activa + 1; // Avanza al siguiente elemento
     }
-    recargarCarrusel();
+    recargarCarrusel(); // Llama a la función para actualizar la posición del carrusel
 }
 
-prev.onclick = function(){
-    if(activa - 1 < 0){ //Ya no hay más espacio para objetos
+// Evento al hacer clic en el botón "prev" 
+prev.onclick = function() {
+    if (activa - 1 < 0) { // Si se encuentra en el primer elemento, vuelve al último
         activa = longitudItems;
-    }else{
-        activa = activa - 1;
+    } else {
+        activa = activa - 1; // Retrocede al elemento anterior
     }
-    recargarCarrusel();
-    }
-
-//Cada 3 segundos, hace la acción de hacer click en un botón de manera automática. 
-let refrescarCarrusel = setInterval(()=>{next.click()},5000)
-function recargarCarrusel(){
-    let checkleft = items[activa].offsetLeft
-    lista.style.left = -checkleft + 'px';
-
-    /*Cuando se presiona algun botón, debe reiniciarse el intervalo en el que
-    el sistema lleva a cabo de manera automática, la acción que este evento desata. esto para
-    que el usuario tenga suficiente tiempo para ver la imagen*/
-    
-    clearInterval(refrescarCarrusel); //Se reinicia el intervalo
-    
-    refrescarCarrusel = setInterval(()=>{next.click()},5000) 
-    /*Se vuelve a definir un intervalo de 5 segundos para llevar a cabo
-    la acción correspondiente cuando se hace click en un botón*/
+    recargarCarrusel(); // Llama a la función para actualizar la posición del carrusel
 }
 
+// Cada 5 segundos, simula un clic en el botón "next" para cambiar de imagen automáticamente
+let refrescarCarrusel = setInterval(() => { next.click() }, 5000);
+
+
+function recargarCarrusel() {
+    let checkleft = items[activa].offsetLeft; // Obtiene la posición izquierda del elemento activo
+    lista.style.left = -checkleft + 'px'; // Mueve la lista de elementos para mostrar el actual
+
+    //Se Cambia el color del texto de los enlaces dependiendo de la imagen activa
+    if (activa === 0) { // Si es la primera imagen, usa la clase "header-alternativo"
+        headerLinks.forEach(link => {
+            link.classList.add('header-alternativo');
+            link.classList.remove('header-blanco');
+        });
+    } else { // Para las demás imágenes, se usa la clase "header-blanco"
+        headerLinks.forEach(link => {
+            link.classList.add('header-blanco');
+            link.classList.remove('header-alternativo');
+        });
+    }
+
+    /* Cuando se presiona algún botón, se debe reiniciar el intervalo del carrusel automático 
+       para que el usuario tenga suficiente tiempo para ver la imagen antes de que cambie automáticamente */
+    
+    clearInterval(refrescarCarrusel); //Detiene/Reinicia el intervalo actual
+    
+    // Se vuelve a definir el intervalo de 5 segundos para continuar el cambio automático de imágenes
+    refrescarCarrusel = setInterval(() => { next.click() }, 5000);
+}
+
+
+
+/*Codigo relacionado con las imagenes del cast */
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".Cuadro").forEach(cuadro => {
         const capa = cuadro.querySelector(".Nombre"); // Seleccionamos el nombre
